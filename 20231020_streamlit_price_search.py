7b93b9c4f5cf291ce45576a1b02917756e35dea4
shakeluna <shakeluna@gmail.com>
2023-10-20 16:57:05 +0900

    Update 20231020_streamlit_price_search.py

--- 20231020_streamlit_price_search.py
+++ 20231020_streamlit_price_search.py
@@ -11,16 +11,30 @@ def get_app_data(appid):
     for row in data:
         if row['steamAppid'] == int(appid):
             return row['name'], row['store'], row['price'], row['link']
-    return "찾을 수 없습니다"
+    return None
 
+def fetch_steam_price(appid):
+    steam_api_url = f"https://store.steampowered.com/api/appdetails?appids={appid}"
+    steam_response = requests.get(steam_api_url).json()
+    if steam_response[str(appid)]['success']:
+        return steam_response[str(appid)]['data']['price_overview']['final_formatted']
+    return "가격 정보 없음"
 
 url = st.text_input("스팀 URL을 입력하세요")
 
 if st.button("검색"):
     appid = url.split("/app/")[1].split("/")[0]
-    name, store, price, link = get_app_data(appid)
-    st.write(f"이름: {name}")
-    st.write(f"상점: {store}")
-    st.write(f"가격: {price}")
-    st.write(f"링크: {link}")
-    st.image(f"https://cdn.cloudflare.steamstatic.com/steam/apps/{appid}/header.jpg")
+    app_data = get_app_data(appid)
+    
+    if app_data:
+        name, store, price, link = app_data
+        st.write(f"이름: {name}")
+        st.write(f"상점: {store}")
+        st.write(f"가격: {price}")
+        st.write(f"링크: {link}")
+        st.image(f"https://cdn.cloudflare.steamstatic.com/steam/apps/{appid}/header.jpg")
+        
+        steam_price = fetch_steam_price(appid)
+        st.write(f"스팀 가격: {steam_price}")
+    else:
+        st.write("찾을 수 없습니다.")
